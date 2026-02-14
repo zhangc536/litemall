81ff42593b8ee29f538fcbf12a580590e2c6c5c6
Your <your.email@example.com>
2026-02-14 19:20:39 +0800

    xiugai

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxPointGoodsController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxPointGoodsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
