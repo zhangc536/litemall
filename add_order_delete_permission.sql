26b7f7fd7a3bd630b30ea3e237d795da23fee823
Your <your.email@example.com>
2026-02-27 15:52:15 +0800

    xiugai

--- add_order_delete_permission.sql
+++ add_order_delete_permission.sql
@@ -1,2 +1,2 @@
-INSERT INTO litemall_permission (role_id, permission, create_time, update_time, deleted) 
+INSERT INTO litemall_permission (role_id, permission, add_time, update_time, deleted) 
 VALUES (2, 'admin:order:delete', NOW(), NOW(), 0);
