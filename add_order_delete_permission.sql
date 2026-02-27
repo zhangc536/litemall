97dfcb33eceed5d48d59ea00bd57bd4c1b562232
Your <your.email@example.com>
2026-02-27 19:47:36 +0800

    xiugai

--- add_order_delete_permission.sql
+++ add_order_delete_permission.sql
@@ -1,2 +1,8 @@
 INSERT INTO litemall_permission (role_id, permission, add_time, update_time, deleted) 
 VALUES (2, 'admin:order:delete', NOW(), NOW(), 0);
+INSERT INTO litemall_permission (role_id, permission, add_time, update_time, deleted) 
+VALUES (2, 'admin:storage:list', NOW(), NOW(), 0);
+INSERT INTO litemall_permission (role_id, permission, add_time, update_time, deleted) 
+VALUES (2, 'admin:storage:create', NOW(), NOW(), 0);
+INSERT INTO litemall_permission (role_id, permission, add_time, update_time, deleted) 
+VALUES (2, 'admin:storage:read', NOW(), NOW(), 0);
